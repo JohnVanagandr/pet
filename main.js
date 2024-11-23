c968a2aa3f5fd11a5cf12d08343f83d56c345b65
john becerra <jfbeccera@gmail.com>
2024-11-23 10:15:45 -0500

    slider testimonials

--- main.js
+++ main.js
@@ -14,70 +14,76 @@ navLinks.addEventListener("click", (e) => {
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
-const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
-};
+// const scrollRevealOption = {
+//   distance: "50px",
+//   origin: "bottom",
+//   duration: 1000,
+// };
 
-ScrollReveal().reveal(".header__content h4", {
-  ...scrollRevealOption,
-});
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".header__content h2", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-ScrollReveal().reveal(".header__btn", {
-  ...scrollRevealOption,
-  delay: 2000,
-});
+// ScrollReveal().reveal(".header__content h4", {
+//   ...scrollRevealOption,
+// });
+// ScrollReveal().reveal(".header__content h1", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
+// ScrollReveal().reveal(".header__content h2", {
+//   ...scrollRevealOption,
+//   delay: 1000,
+// });
+// ScrollReveal().reveal(".header__content p", {
+//   ...scrollRevealOption,
+//   delay: 1500,
+// });
+// ScrollReveal().reveal(".header__btn", {
+//   ...scrollRevealOption,
+//   delay: 2000,
+// });
 
-ScrollReveal().reveal(".intro__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
+// ScrollReveal().reveal(".intro__card", {
+//   ...scrollRevealOption,
+//   interval: 500,
+// });
 
-ScrollReveal().reveal(
-  ".about__row:nth-child(3) .about__image img, .about__row:nth-child(5) .about__image img",
-  {
-    ...scrollRevealOption,
-    origin: "left",
-  }
-);
-ScrollReveal().reveal(".about__row:nth-child(4) .about__image img", {
-  ...scrollRevealOption,
-  origin: "right",
-});
-ScrollReveal().reveal(".about__content span", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".about__content h4", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-ScrollReveal().reveal(".about__content p", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
+// ScrollReveal().reveal(
+//   ".about__row:nth-child(3) .about__image img, .about__row:nth-child(5) .about__image img",
+//   {
+//     ...scrollRevealOption,
+//     origin: "left",
+//   }
+// );
+// ScrollReveal().reveal(".about__row:nth-child(4) .about__image img", {
+//   ...scrollRevealOption,
+//   origin: "right",
+// });
+// ScrollReveal().reveal(".about__content span", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
+// ScrollReveal().reveal(".about__content h4", {
+//   ...scrollRevealOption,
+//   delay: 1000,
+// });
+// ScrollReveal().reveal(".about__content p", {
+//   ...scrollRevealOption,
+//   delay: 1500,
+// });
 
-ScrollReveal().reveal(".product__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
+// ScrollReveal().reveal(".product__card", {
+//   ...scrollRevealOption,
+//   interval: 500,
+// });
 
-ScrollReveal().reveal(".service__card", {
-  duration: 1000,
-  interval: 500,
-});
+// ScrollReveal().reveal(".service__card", {
+//   duration: 1000,
+//   interval: 500,
+// });
+
+
+// ScrollReveal().reveal(".instagram__grid img", {
+//   duration: 1000,
+//   interval: 500,
+// });
 
 const swiper = new Swiper(".swiper", {
   slidesPerView: 3,
@@ -85,7 +91,3 @@ const swiper = new Swiper(".swiper", {
   loop: true,
 });
 
-ScrollReveal().reveal(".instagram__grid img", {
-  duration: 1000,
-  interval: 500,
-});
